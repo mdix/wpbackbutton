7605451c6c4b509a91d087469e43a9db7ea5c42e
mdix <dix@mediaman.de>
2012-03-05 09:29:36 +0100

    Replaced tabs with spaces

--- plugin.php
+++ plugin.php
@@ -10,140 +10,140 @@ Author Email: marcdix@marcdix.de
 */
 class WP_back_button {
 
-	/*--------------------------------------------*
-	 * Constants
-	 *--------------------------------------------*/
+    /*--------------------------------------------*
+     * Constants
+     *--------------------------------------------*/
 
-	const name = 'wpbackbutton';
+    const name = 'wpbackbutton';
 
-	const slug = 'wpbackbutton';
+    const slug = 'wpbackbutton';
 
-	/*--------------------------------------------*
-	 * Constructor
-	 *--------------------------------------------*/
+    /*--------------------------------------------*
+     * Constructor
+     *--------------------------------------------*/
 
-	/**
-	 * Initializes the plugin by setting localization, filters, and administration functions.
-	 */
-	function __construct() {
+    /**
+     * Initializes the plugin by setting localization, filters, and administration functions.
+     */
+    function __construct() {
 
-	    // Define constants used throughout the plugin
-	    $this->init_plugin_constants();
+        // Define constants used throughout the plugin
+        $this->init_plugin_constants();
 
-		load_plugin_textdomain( PLUGIN_LOCALE, false, dirname( plugin_basename( __FILE__ ) ) . '/lang' );
+        load_plugin_textdomain( PLUGIN_LOCALE, false, dirname( plugin_basename( __FILE__ ) ) . '/lang' );
 
-    	// Load JavaScript and stylesheets
-    	$this->register_scripts_and_styles();
+        // Load JavaScript and stylesheets
+        $this->register_scripts_and_styles();
 
-	    /*
-	     * TODO:
-	     * Define the custom functionality for your plugin. The first parameter of the
-	     * add_action/add_filter calls are the hooks into which your code should fire.
-	     *
-	     * The second parameter is the function name located within this class. See the stubs
-	     * later in the file.
-	     *
-	     * For more information:
-	     * http://codex.wordpress.org/Plugin_API#Hooks.2C_Actions_and_Filters
-	     */
-	    add_action( 'TODO', array( $this, 'action_method_name' ) );
-	    add_filter( 'TODO', array( $this, 'filter_method_name' ) );
+        /*
+         * TODO:
+         * Define the custom functionality for your plugin. The first parameter of the
+         * add_action/add_filter calls are the hooks into which your code should fire.
+         *
+         * The second parameter is the function name located within this class. See the stubs
+         * later in the file.
+         *
+         * For more information:
+         * http://codex.wordpress.org/Plugin_API#Hooks.2C_Actions_and_Filters
+         */
+        add_action( 'TODO', array( $this, 'action_method_name' ) );
+        add_filter( 'TODO', array( $this, 'filter_method_name' ) );
 
-	} // end constructor
+    } // end constructor
 
-	/*--------------------------------------------*
-	 * Core Functions
-	 *---------------------------------------------*/
+    /*--------------------------------------------*
+     * Core Functions
+     *---------------------------------------------*/
 
-	/**
- 	 * Note:  Actions are points in the execution of a page or process
-	 *        lifecycle that WordPress fires.
-	 *
-	 *		  WordPress Actions: http://codex.wordpress.org/Plugin_API#Actions
-	 *		  Action Reference:  http://codex.wordpress.org/Plugin_API/Action_Reference
-	 *
-	 */
-	function action_method_name() {
-    	// TODO define your action method here
-	} // end action_method_name
+    /**
+      * Note:  Actions are points in the execution of a page or process
+     *        lifecycle that WordPress fires.
+     *
+     *          WordPress Actions: http://codex.wordpress.org/Plugin_API#Actions
+     *          Action Reference:  http://codex.wordpress.org/Plugin_API/Action_Reference
+     *
+     */
+    function action_method_name() {
+        // TODO define your action method here
+    } // end action_method_name
 
-	/**
-	 * Note:  Filters are points of execution in which WordPress modifies data
-	 *        before saving it or sending it to the browser.
-	 *
-	 *		  WordPress Filters: http://codex.wordpress.org/Plugin_API#Filters
-	 *		  Filter Reference:  http://codex.wordpress.org/Plugin_API/Filter_Reference
-	 *
-	 */
-	function filter_method_name() {
-	    // TODO define your filter method here
-	} // end filter_method_name
+    /**
+     * Note:  Filters are points of execution in which WordPress modifies data
+     *        before saving it or sending it to the browser.
+     *
+     *          WordPress Filters: http://codex.wordpress.org/Plugin_API#Filters
+     *          Filter Reference:  http://codex.wordpress.org/Plugin_API/Filter_Reference
+     *
+     */
+    function filter_method_name() {
+        // TODO define your filter method here
+    } // end filter_method_name
 
-	/*--------------------------------------------*
-	 * Private Functions
-	 *---------------------------------------------*/
+    /*--------------------------------------------*
+     * Private Functions
+     *---------------------------------------------*/
 
-	/**
-	 * Initializes constants used for convenience throughout
-	 * the plugin.
-	 */
-	private function init_plugin_constants() {
+    /**
+     * Initializes constants used for convenience throughout
+     * the plugin.
+     */
+    private function init_plugin_constants() {
 
-		/*
-		 * This is what shows in the Widgets area of WordPress.
-		 */
-		define( 'PLUGIN_NAME', self::name );
+        /*
+         * This is what shows in the Widgets area of WordPress.
+         */
+        define( 'PLUGIN_NAME', self::name );
 
-		/*
-		 * this is the slug of your plugin used in initializing it with
-		 * the WordPress API.
+        /*
+         * this is the slug of your plugin used in initializing it with
+         * the WordPress API.
 
-		 * This should also be the
-		 * directory in which your plugin resides. Use hyphens.
-		 *
-		 */
-		define( 'PLUGIN_SLUG', self::slug );
+         * This should also be the
+         * directory in which your plugin resides. Use hyphens.
+         *
+         */
+        define( 'PLUGIN_SLUG', self::slug );
 
 
-	} // end init_plugin_constants
+    } // end init_plugin_constants
 
-	/**
-	 * Registers and enqueues stylesheets for the administration panel and the
-	 * public facing site.
-	 */
-	private function register_scripts_and_styles() {
-		if ( is_admin() ) {
-			$this->load_file( self::slug . '-admin-script', '/js/admin.js', true );
-			$this->load_file( self::slug . '-admin-style', '/css/admin.css' );
-		} else {
-			$this->load_file( self::slug . '-script', '/js/widget.js', true );
-			$this->load_file( self::slug . '-style', '/css/widget.css' );
-		} // end if/else
-	} // end register_scripts_and_styles
+    /**
+     * Registers and enqueues stylesheets for the administration panel and the
+     * public facing site.
+     */
+    private function register_scripts_and_styles() {
+        if ( is_admin() ) {
+            $this->load_file( self::slug . '-admin-script', '/js/admin.js', true );
+            $this->load_file( self::slug . '-admin-style', '/css/admin.css' );
+        } else {
+            $this->load_file( self::slug . '-script', '/js/widget.js', true );
+            $this->load_file( self::slug . '-style', '/css/widget.css' );
+        } // end if/else
+    } // end register_scripts_and_styles
 
-	/**
-	 * Helper function for registering and enqueueing scripts and styles.
-	 *
-	 * @name	The 	ID to register with WordPress
-	 * @file_path		The path to the actual file
-	 * @is_script		Optional argument for if the incoming file_path is a JavaScript source file.
-	 */
-	private function load_file( $name, $file_path, $is_script = false ) {
+    /**
+     * Helper function for registering and enqueueing scripts and styles.
+     *
+     * @name    The     ID to register with WordPress
+     * @file_path        The path to the actual file
+     * @is_script        Optional argument for if the incoming file_path is a JavaScript source file.
+     */
+    private function load_file( $name, $file_path, $is_script = false ) {
 
-		$url = plugins_url($file_path, __FILE__);
-		$file = plugin_dir_path(__FILE__) . $file_path;
+        $url = plugins_url($file_path, __FILE__);
+        $file = plugin_dir_path(__FILE__) . $file_path;
 
-		if( file_exists( $file ) ) {
-			if( $is_script ) {
-				wp_register_script( $name, $url, array('jquery') );
-				wp_enqueue_script( $name );
-			} else {
-				wp_register_style( $name, $url );
-				wp_enqueue_style( $name );
-			} // end if
-		} // end if
+        if( file_exists( $file ) ) {
+            if( $is_script ) {
+                wp_register_script( $name, $url, array('jquery') );
+                wp_enqueue_script( $name );
+            } else {
+                wp_register_style( $name, $url );
+                wp_enqueue_style( $name );
+            } // end if
+        } // end if
 
-	} // end load_file
+    } // end load_file
 
 } // end class
 
