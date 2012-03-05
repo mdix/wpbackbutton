7ee46ae9bb1b8e9845572d909f8db7d4022b6ce3
mdix <dix@mediaman.de>
2012-03-05 09:21:46 +0100

    Added plugin info, removed some TODOs

--- plugin.php
+++ plugin.php
@@ -1,18 +1,18 @@
 <?php
 /*
-Plugin Name: TODO
+Plugin Name: wpbackbutton
 Plugin URI: TODO
-Description: TODO
-Version: 1.0
-Author: TODO
-Author URI: TODO
-Author Email: TODO
+Description: Enables a back button that reverse engineers the users navigation to enable a simple "back" button on every page.
+Version: 0.1
+Author: Marc Dix
+Author URI: http://www.dixpix.de
+Author Email: marcdix@marcdix.de
 License:
 
   Copyright 2011 TODO (email@domain.com)
 
   This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License, version 2, as 
+  it under the terms of the GNU General Public License, version 2, as
   published by the Free Software Foundation.
 
   This program is distributed in the hope that it will be useful,
@@ -23,41 +23,35 @@ License:
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-  
-*/
 
-// TODO: rename this class to a proper name for yuour plugin
-class PluginName {
-	
+*/
+class WP_back_button {
+
 	/*--------------------------------------------*
 	 * Constants
 	 *--------------------------------------------*/
-	
-	/**
-	 * TODO: update these values to reflect the name  and slug of your plugin.
-	 */
 
-	const name = 'Plugin Name';
-	
-	const slug = 'plugin-name-slug';
-	 
+	const name = 'wpbackbutton';
+
+	const slug = 'wpbackbutton';
+
 	/*--------------------------------------------*
 	 * Constructor
 	 *--------------------------------------------*/
-	
+
 	/**
 	 * Initializes the plugin by setting localization, filters, and administration functions.
 	 */
 	function __construct() {
-	
+
 	    // Define constants used throughout the plugin
 	    $this->init_plugin_constants();
-  
+
 		load_plugin_textdomain( PLUGIN_LOCALE, false, dirname( plugin_basename( __FILE__ ) ) . '/lang' );
-		
+
     	// Load JavaScript and stylesheets
     	$this->register_scripts_and_styles();
-		
+
 	    /*
 	     * TODO:
 	     * Define the custom functionality for your plugin. The first parameter of the
@@ -66,18 +60,18 @@ class PluginName {
 	     * The second parameter is the function name located within this class. See the stubs
 	     * later in the file.
 	     *
-	     * For more information: 
+	     * For more information:
 	     * http://codex.wordpress.org/Plugin_API#Hooks.2C_Actions_and_Filters
 	     */
 	    add_action( 'TODO', array( $this, 'action_method_name' ) );
 	    add_filter( 'TODO', array( $this, 'filter_method_name' ) );
 
 	} // end constructor
-	
+
 	/*--------------------------------------------*
 	 * Core Functions
 	 *---------------------------------------------*/
-	
+
 	/**
  	 * Note:  Actions are points in the execution of a page or process
 	 *        lifecycle that WordPress fires.
@@ -89,7 +83,7 @@ class PluginName {
 	function action_method_name() {
     	// TODO define your action method here
 	} // end action_method_name
-	
+
 	/**
 	 * Note:  Filters are points of execution in which WordPress modifies data
 	 *        before saving it or sending it to the browser.
@@ -101,44 +95,35 @@ class PluginName {
 	function filter_method_name() {
 	    // TODO define your filter method here
 	} // end filter_method_name
-  
+
 	/*--------------------------------------------*
 	 * Private Functions
 	 *---------------------------------------------*/
-   
+
 	/**
-	 * Initializes constants used for convenience throughout 
+	 * Initializes constants used for convenience throughout
 	 * the plugin.
 	 */
 	private function init_plugin_constants() {
-		
-		/* TODO
-		 * 
-		 * Define this as the name of your plugin. This is what shows
-		 * in the Widgets area of WordPress.
-		 * 
-		 * For example: WordPress Widget Boilerplate.
+
+		/*
+		 * This is what shows in the Widgets area of WordPress.
 		 */
-		if ( !defined( 'PLUGIN_NAME' ) ) {
-		  define( 'PLUGIN_NAME', self::name );
-		} // end if
-		
-		/* TODO
-		 * 
+		define( 'PLUGIN_NAME', self::name );
+
+		/*
 		 * this is the slug of your plugin used in initializing it with
 		 * the WordPress API.
-		 
+
 		 * This should also be the
 		 * directory in which your plugin resides. Use hyphens.
-		 * 
-		 * For example: wordpress-widget-boilerplate
+		 *
 		 */
-		if ( !defined( 'PLUGIN_SLUG' ) ) {
-		  define( 'PLUGIN_SLUG', self::slug );
-		} // end if
-	
+		define( 'PLUGIN_SLUG', self::slug );
+
+
 	} // end init_plugin_constants
-	
+
 	/**
 	 * Registers and enqueues stylesheets for the administration panel and the
 	 * public facing site.
@@ -147,12 +132,12 @@ class PluginName {
 		if ( is_admin() ) {
 			$this->load_file( self::slug . '-admin-script', '/js/admin.js', true );
 			$this->load_file( self::slug . '-admin-style', '/css/admin.css' );
-		} else { 
+		} else {
 			$this->load_file( self::slug . '-script', '/js/widget.js', true );
 			$this->load_file( self::slug . '-style', '/css/widget.css' );
 		} // end if/else
 	} // end register_scripts_and_styles
-	
+
 	/**
 	 * Helper function for registering and enqueueing scripts and styles.
 	 *
@@ -161,7 +146,7 @@ class PluginName {
 	 * @is_script		Optional argument for if the incoming file_path is a JavaScript source file.
 	 */
 	private function load_file( $name, $file_path, $is_script = false ) {
-		
+
 		$url = plugins_url($file_path, __FILE__);
 		$file = plugin_dir_path(__FILE__) . $file_path;
 
@@ -174,10 +159,10 @@ class PluginName {
 				wp_enqueue_style( $name );
 			} // end if
 		} // end if
-    
+
 	} // end load_file
-  
+
 } // end class
-// TODO: update the instantiation call of your plugin to the name given at the class definition
-new PluginName();
+
+new WP_back_button();
 ?>
